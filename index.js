b2989fa9f54c254b09f56228d026b3533ed865ee
jehendeoff <jehende@jehende.fr>
2020-08-08 18:19:09 +0200

    comment + chaging the rate limiter to 5s + using a function for the http(s) handler

--- index.js
+++ index.js
@@ -7,33 +7,42 @@
 
 //In future revision I plan on adding support for socket.io !
 
+//check if it can boot
 const fs = require("fs")
 if(!fs.existsSync("./function/")) return console.error("You installed it wrong, the \"function\" folder is missing.\n\n")
 
+//starting the boot sequence
 let config
 fs.existsSync("./function/bootup.js") ? config = require("./function/bootup").run() : () => {console.error("You installed it wrong, the \"bootup\" file is missing.\n\n"); process.exit(1)}
+//simple check to verifie the config has been passed
 config.path ? console.log('config, verified & loaded') : () => {throw new Error("Config wasn't able to be loaded")}
 
 var limit = {}
-
+//the rate limiter
 setInterval(_ =>{
-    console.log(limit)
     for (var key in limit) {
         if (limit.hasOwnProperty(key))
             if(limit[key].count > 0){
                 limit[key] = {
-                    count: limit[key].count - 1
+                    count: limit[key].count - 5
                 }
             }
     }
-}, 1 * 1000)
+}, 5 * 1000)
 
-const http = require("http")
-
-http.createServer((req, res) => {
+/**
+ * @param {https.IncomingMessage} req The request
+ * @param {http.ServerResponse} res The response
+ * The handler for http and https requests
+ */
+function handler (req, res){
+    
+    //checking if rate limit is on
     if(config.rate_limit === true){
         if(limit[req.connection.remoteAddress]){
+            //checkinf it's above or not
             if (parseInt(limit[req.connection.remoteAddress].count)>= config.rate_limit_number){
+                //checking if it should increase it anyway (rate_limite_continue)
                 if(config.rate_limit_continue === true){
                     limit[req.connection.remoteAddress]= {
                         count: limit[req.connection.remoteAddress].count + config.rate_limit_continue_rate
@@ -61,7 +70,9 @@ http.createServer((req, res) => {
             }
         }
     }
-    if(!req.headers.host || !fs.existsSync(config.path + req.headers.host)){
+
+    //ckecking the headers
+    if(!req.headers.host){
         
         res.writeHead(400, {
             'X-Frame-Options': 'DENY',
@@ -73,12 +84,33 @@ http.createServer((req, res) => {
         return
 
     }
-    else
-    {
-        require(config.path + req.headers.host + '/index.js').http(req, res, config)
+
+    //forgetting the port
+    var domain = `${req.headers.host}`.split(":")[0]
+
+    //checking if domain is registered
+    if (!fs.existsSync(config.path + req.headers.host)){
+        res.writeHead(500, {
+            'X-Frame-Options': 'DENY',
+            'content-type': 'text/html;charset=utf-8',
+            'Cache-Control': 'max-age=31536000'
+        });
+        res.write(fs.readFileSync("./Default web page/domain not found.html"))
+        res.end();
+        return
     }
+
+    //passing the info
+    require(config.path + req.headers.host + '/index.js').http(req, res, config)
+}
+
+//Creating the http server
+const http = require("http")
+http.createServer((req, res) => {
+    handler(req, res)
 }).listen(config.http_port)
 
+//creating the https server only if https is true
 let https
 let httpsoptions
 if (config.https === true) https = require("https")
@@ -87,50 +119,5 @@ if (config.https === true) httpsoptions = {
     cert: fs.readFileSync(config.path_to_ssl_cert + "cert.pem")
 };
 if (config.https === true) https.createServer(httpsoptions, (req, res) => {
-    if(config.rate_limit === true){
-        if(limit[req.connection.remoteAddress]){
-            if (parseInt(limit[req.connection.remoteAddress].count)>= config.rate_limit_number){
-                if(config.rate_limit_continue === true){
-                    limit[req.connection.remoteAddress]= {
-                        count: limit[req.connection.remoteAddress].count + config.rate_limit_continue_rate
-                    }
-                }
-                res.writeHead(429, {
-                    'X-Frame-Options': 'DENY',
-                    'content-type': 'text/html;charset=utf-8',
-                    'Cache-Control': 'max-age=10',
-                    'Retry-After': '10',
-                    'Tk': 'N'
-                });
-                res.write(fs.readFileSync("./Default web page/too many request.html"));
-                res.end();
-                return
-            }else{
-                limit[req.connection.remoteAddress]= {
-                    count: limit[req.connection.remoteAddress].count + 1
-                }
-            }
-    
-        }else{
-            limit[req.connection.remoteAddress]= {
-                count: 1
-            }
-        }
-    }
-    if(!req.headers.host || !fs.existsSync(config.path + req.headers.host)){
-        
-        res.writeHead(400, {
-            'X-Frame-Options': 'DENY',
-            'content-type': 'text/html;charset=utf-8',
-            'Cache-Control': 'max-age=31536000'
-        });
-        res.write(fs.readFileSync("./Default web page/bad host.html"))
-        res.end();
-        return
-
-    }
-    else
-    {
-        require(config.path + req.headers.host + '/index.js').https(req, res, config)
-    }
+    handler(req, res)
 }).listen(config.https_port)
