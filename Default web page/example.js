a3ee369dc7922f5e0a4506d51ca99ab588f20464
Jéhèndé <jehende@jehende.fr>
2022-05-22 14:15:07 +0200

    ;

--- Default web page/example.js
+++ Default web page/example.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const http = require('http')
-const io = require('$dir\\node_modules\\socket.io')
+const fs = require("fs");
+const http = require("http");
+const SocketIO = require("socket.io");
 /**
  * @param {http.IncomingMessage} req The request
  * @param {http.ServerResponse} res The response
@@ -8,21 +8,21 @@ const io = require('$dir\\node_modules\\socket.io')
  * @param {String} domain The parsed config
  */
 exports.http = async (req, res, config, domain) => {
-    res.writeHead(200, {
-        'X-Frame-Options': 'DENY',
-        'X-Coded-By': 'Jehende',
-        'content-type': 'text/html;charset=utf-8',
-        'Cache-Control': 'max-age=31536000'
-    });
-    res.write(fs.readFileSync(config.path + domain + "\\web\\index.html"));
-    res.end();
-    return;
-}
+	res.writeHead(200, {
+		"X-Frame-Options": "DENY",
+		"X-Coded-By": "Jehende",
+		"content-type": "text/html;charset=utf-8",
+		"Cache-Control": "max-age=31536000"
+	});
+	res.write(fs.readFileSync(__dirname + "\\web\\index.html"));
+	res.end();
+	return;
+};
 /**
  * @param {SocketIO.Socket} socket The socket
  * @param {String} config The parsed config
  * @param {String} domain The parsed config
  */
 exports.socket = async (socket, config, domain) => {
-    socket.emit("success", "Socketio connected")
-}
+	socket.emit("success", "Socketio connected");
+};
